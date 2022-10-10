28bd443e11b68e9e99438aa3a44dfe4ce8c2de57
hyejinlim <yoon1440_@naver.com>
2022-10-10 13:48:30 +0900

    🔥 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import * as styles from './styles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Build Test <br /> MODE: {process.env.REACT_APP_MODE} <br />
-          API_URL: {process.env.REACT_APP_API_URL}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div css={styles.text}>Text Styling</div>
-      </header>
-    </div>
-  );
+  return <div css={styles.text}>Text Styling</div>;
 }
 
 export default App;
